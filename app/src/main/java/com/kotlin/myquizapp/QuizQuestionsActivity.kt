33f26a3a650e4cc578e0862a47a776c8ecbea582
nicolaspc-dev <nicolaspc.wd@gmail.com>
2022-11-14 11:29:40 -0500

    Comments

--- app/src/main/java/com/kotlin/myquizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/kotlin/myquizapp/QuizQuestionsActivity.kt
@@ -6,24 +6,24 @@ import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.*
 import androidx.core.content.ContextCompat
 
 class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
-  private var mCurrentPosition: Int = 1
-  private var mQuestionsList: ArrayList<Question>? = null
-  private var mSelectedOptionPosition: Int = 0
+  // Global variables
   private var mUserName: String? = null
+  private var mQuestionsList: ArrayList<Question>? = null
+  private var mCurrentPosition: Int = 1
+  private var mSelectedOptionPosition: Int = 0
   private var mCorrectAnswers: Int = 0
 
+  // Widgets declaration
   private var progressBar: ProgressBar? = null
   private var tvProgress: TextView? = null
   private var tvQuestion: TextView? = null
   private var ivImage: ImageView? = null
-
   private var tvOptionOne: TextView? = null
   private var tvOptionTwo: TextView? = null
   private var tvOptionThree: TextView? = null
@@ -35,8 +35,10 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_quiz_questions)
 
+    // Username Init
     mUserName = intent.getStringExtra(Constants.USER_NAME)
 
+    // Widgets Init
     progressBar = findViewById(R.id.progressBar)
     tvProgress = findViewById(R.id.tv_progress)
     tvQuestion = findViewById(R.id.tv_question)
@@ -47,30 +49,42 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     tvOptionFour = findViewById(R.id.tv_option_four)
     btnSubmit = findViewById(R.id.btn_submit)
 
-
+    // Widgets functionality by "click listener"
     tvOptionOne?.setOnClickListener(this)
     tvOptionTwo?.setOnClickListener(this)
     tvOptionThree?.setOnClickListener(this)
     tvOptionFour?.setOnClickListener(this)
     btnSubmit?.setOnClickListener(this)
 
+    // Questions array Init
     mQuestionsList = Constants.getQuestions()
 
+    // Function Execution
     setQuestion()
   }
 
+  @SuppressLint("SetTextI18n")
   private fun setQuestion() {
+    // Reset styles
     defaultOptionsView()
+
+    // QuestionView constructor
+    // Select the current question by index position.
     val question: Question = mQuestionsList!![mCurrentPosition - 1]
+    // Display current image.
     ivImage?.setImageResource(question.image)
+    // Update the progress bar.
     progressBar?.progress = mCurrentPosition
+    // Update the textView progress.
     tvProgress?.text = "$mCurrentPosition/${progressBar?.max}"
+    // Set widgets question and options text.
     tvQuestion?.text = question.question
     tvOptionOne?.text = question.optionOne
     tvOptionTwo?.text = question.optionTwo
     tvOptionThree?.text = question.optionThree
     tvOptionFour?.text = question.optionFour
 
+    // Conditional: let the app know if we are at the final question.
     if(mCurrentPosition == mQuestionsList!!.size){
       btnSubmit?.text = "FINISH"
     }else{
@@ -79,7 +93,10 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
   }
 
   private fun defaultOptionsView(){
+    // Empty array Declaration
     val options = ArrayList<TextView>()
+
+    // Array Filling with options
     tvOptionOne?.let{
       options.add(0, it)
     }
@@ -93,6 +110,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
       options.add(3, it)
     }
 
+    // Iteration: for each View, set "default" styles.
     for(option in options){
       option.setTextColor(Color.parseColor("#7A8089"))
       option.typeface = Typeface.DEFAULT
@@ -100,12 +118,14 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
         this, R.drawable.default_option_border_bg
       )
     }
-
   }
 
+  // Set styles for the options menu while selecting.
   private fun selectedOptionView(tv: TextView, selectedOptionNum: Int ){
     defaultOptionsView()
+    // Update the current "selectedOption".
     mSelectedOptionPosition = selectedOptionNum
+    // Styles
     tv.setTextColor(Color.parseColor("#363A43"))
     tv.setTypeface(tv.typeface, Typeface.BOLD)
     tv.background = ContextCompat.getDrawable(
@@ -113,6 +133,8 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     )
   }
 
+  // Setting the functionality of the click event.
+  @SuppressLint("SetTextI18n")
   override fun onClick(view: View?) {
     when(view?.id){
       R.id.tv_option_one -> {
@@ -153,12 +175,16 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
           }
         }else{
           val question = mQuestionsList?.get(mCurrentPosition - 1)
+          // Conditional: Set "Wrong" Answer Style || Score Update.
           if(question!!.correctAnswer != mSelectedOptionPosition){
             answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
           }else{
             mCorrectAnswers++
           }
+          // Set the "Right" Answer style.
           answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+
+          // Conditional: Update button text at the end of the quiz.
           if(mCurrentPosition == mQuestionsList!!.size) {
             btnSubmit?.text = "FINISH"
           }else{
